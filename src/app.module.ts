acf977f9140e6809a3413ba72bd360b17deffe2b
Rafaela Atanasio <rah.jd@hotmail.com>
2024-07-01 14:45:47 -0300

    Criada as pastas cliente, conta e gerente com os módulos, serviços e controladores

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClienteModule } from './cliente/cliente.module';
+import { GerenteModule } from './gerente/gerente.module';
+import { ContaModule } from './conta/conta.module';
 
 @Module({
-  imports: [],
+  imports: [ClienteModule, GerenteModule, ContaModule],
   controllers: [AppController],
   providers: [AppService],
 })
