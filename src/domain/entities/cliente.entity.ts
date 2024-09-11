f984cab9767096f1db16411a54051f309af7fd6b
Rafaela Atanasio <rah.jd@hotmail.com>
2024-09-11 20:03:42 -0300

    Mudando o formato das classes para inserir em tabelas e colunas no banco de dados

--- src/domain/entities/cliente.entity.ts
+++ src/domain/entities/cliente.entity.ts
@@ -2,12 +2,23 @@ import { Conta } from "./conta.entity";
 
 export class Cliente {
   public contas: Conta[] = [];
+  public nome: string;
+  public id: number;
+  public endereco: string;
+  public telefone: string;
+  public rendaSalarial: number
 
   constructor(
-    public nome: string,
-    public id: number,
-    public endereco: string,
-    public telefone: string,
-    public rendaSalarial: number
-  ) { }
+    nome: string,
+    id: number,
+    endereco: string,
+    telefone: string,
+    rendaSalarial: number
+  ) {
+    this.nome = nome;
+    this.id = id;
+    this.endereco = endereco;
+    this.telefone = telefone;
+    this.rendaSalarial = rendaSalarial
+  }
 }
