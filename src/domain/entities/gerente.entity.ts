7ea94f8cc96142d2ec99374a5923d17a49ae6604
Rafaela Atanasio <rah.jd@hotmail.com>
2024-09-11 20:11:20 -0300

    Mudando o formato das classes gerente.entity.ts

--- src/domain/entities/gerente.entity.ts
+++ src/domain/entities/gerente.entity.ts
@@ -4,40 +4,48 @@ import { ContaCorrente } from "./contaCorrente.entity";
 import { ContaPoupanca } from "./contaPoupanca.entity";
 
 export class Gerente {
-    constructor(
-        public nome: string,
-        public id: number,
+        public nome: string;
+        public id: number;
         public clientes: Cliente[] = []
-    ) { }
 
-    adicionarCliente(cliente: Cliente): void {
-        this.clientes.push(cliente);
+    constructor(
+        nome: string,
+        id: number,
+        clientes: Cliente[] = []
+    ) { 
+        this.nome = nome,
+        this.id = id,
+        this.clientes = clientes = []
+}
+
+adicionarCliente(cliente: Cliente): void {
+    this.clientes.push(cliente);
+}
+
+removerCliente(clienteId: number): void {
+    this.clientes = this.clientes.filter(cliente => cliente.id !== clienteId);
+}
+
+abrirConta(tipo: 'corrente' | 'poupanca', cliente: Cliente): Conta {
+    let novaConta: Conta;
+    if (tipo === 'corrente') {
+        novaConta = new ContaCorrente(cliente.id, cliente);
+    } else {
+        novaConta = new ContaPoupanca(cliente.id, cliente);
     }
+    cliente.contas.push(novaConta);
+    return novaConta;
+}
 
-    removerCliente(clienteId: number): void {
-        this.clientes = this.clientes.filter(cliente => cliente.id !== clienteId);
-    }
+fecharConta(cliente: Cliente, contaNumero: number): void {
+    cliente.contas = cliente.contas.filter(conta => conta.numero !== contaNumero);
+}
 
-    abrirConta(tipo: 'corrente' | 'poupanca', cliente: Cliente): Conta {
-        let novaConta: Conta;
-        if (tipo === 'corrente') {
-            novaConta = new ContaCorrente(cliente.id, cliente);
-        } else {
-            novaConta = new ContaPoupanca(cliente.id, cliente);
-        }
-        cliente.contas.push(novaConta);
-        return novaConta;
-    }
+modificarTipoConta(cliente: Cliente, contaNumero: number, novoTipo: 'corrente' | 'poupanca'): Conta {
+    const conta = cliente.contas.find(conta => conta.numero === contaNumero);
+    if (!conta) throw new Error("Conta não encontrada.");
 
-    fecharConta(cliente: Cliente, contaNumero: number): void {
-        cliente.contas = cliente.contas.filter(conta => conta.numero !== contaNumero);
-    }
-
-    modificarTipoConta(cliente: Cliente, contaNumero: number, novoTipo: 'corrente' | 'poupanca'): Conta {
-        const conta = cliente.contas.find(conta => conta.numero === contaNumero);
-        if (!conta) throw new Error("Conta não encontrada.");
-
-        this.fecharConta(cliente, contaNumero);
-        return this.abrirConta(novoTipo, cliente);
-    }
+    this.fecharConta(cliente, contaNumero);
+    return this.abrirConta(novoTipo, cliente);
+}
 }
