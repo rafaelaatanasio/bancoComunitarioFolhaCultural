49efef8498b16e6572a9c3a4819f8bdab8568067
Rafaela Atanasio <rah.jd@hotmail.com>
2024-09-12 19:15:33 -0300

    Configurado TypeORM no NestJS via app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Cliente } from './domain/entities/cliente.entity';
+import { Conta } from './domain/entities/conta.entity';
+import { ContaCorrente } from './domain/entities/contaCorrente.entity';
+import { ContaPoupanca } from './domain/entities/contaPoupanca.entity';
+import { Gerente } from './domain/entities/gerente.entity';
+import { ClientesService } from './domain/services/cliente.service';
 import { DomainModule } from './domain/domain.module';
 import { ApplicationModule } from './application/application.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -12,11 +18,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       database: 'users',
       username: 'rafaela',
       password: 'User123',
-      entities: [],
+      entities: [Cliente, Conta, ContaCorrente, ContaPoupanca, Gerente],
       synchronize: true,
     }),
     DomainModule, ApplicationModule],
   controllers: [],
-  providers: []
+  providers: [ClientesService]
 })
 export class AppModule { }
